7a09ff38d430967812b5a65acec4792e66e0874b
stefan11111 <stefan11111@shitposting.expert>
2024-08-02 00:44:35 +0300

    update itstool program

--- itstool/itstool.c
+++ itstool/itstool.c
@@ -3,61 +3,69 @@
 #include <string.h>
 #include <sys/stat.h>
 
+#define MASK 00755
+#define PATH_MAX 4096
+
+static void mkdir_p_and_touch (char *path)
+{
+    for(char *ptr = strchr(path, '/'); ptr; ptr = strchr(ptr, '/')) {
+        *ptr = '\0';
+        mkdir(path, MASK);
+        *ptr = '/';
+        ptr++;
+    }
+    FILE *f = fopen(path, "w");
+    fclose(f);
+}
+
+static void mkdir_p (char *path)
+{
+    for(char *ptr = strchr(path, '/'); ptr; ptr = strchr(ptr, '/')) {
+        *ptr = '\0';
+        mkdir(path, MASK);
+        *ptr = '/';
+        ptr++;
+    }
+    mkdir(path, MASK);
+}
+
 int main(int argc, char **argv)
 {
     if(argc < 2) {
         return 0;
     }
 
-    for(char **p = argv; *p; p++) {
-        if (!strncmp(*p, "-o", sizeof("-o") - 1) || !strncmp(*p, "--out", sizeof("--out") - 1)) {
-            p++;
-            char *ptr = strrchr(*p, '/');
-            if (ptr) {
-                *ptr = '\0';
-                mkdir(*p, 00644);
-                ptr++;
-            }
-            else {
-                ptr = *p;
-            }
-
-            FILE *f = fopen(ptr, "w");
-            fclose(f);
-            return 0;
-        }
-    }
+    char merge_flag = 0;
 
     for(char **p = argv; *p; p++) {
         if (!strncmp(*p, "-m", sizeof("-m") - 1) || !strncmp(*p, "--merge", sizeof("--merge") - 1)) {
-            p++;
-            char *ptr = strrchr(*p, '/');
-            if (ptr) {
-                *ptr = '\0';
-                mkdir(*p, 00644);
-                ptr++;
-            }
-            else {
-                ptr = *p;
-            }
-
-            FILE *f = fopen(ptr, "w");
-            fclose(f);
-            return 0;
+            merge_flag = 1;
         }
     }
 
-    char *ptr = strrchr(argv[argc - 1], '/');
-    if (ptr) {
-        *ptr = '\0';
-        mkdir(argv[argc - 1], 00644);
-        ptr++;
-    }
-    else {
-        ptr = argv[argc - 1];
+    if (merge_flag == 0) {
+        for(char **p = argv; *p; p++) {
+            if (!strncmp(*p, "-o", sizeof("-o") - 1) || !strncmp(*p, "--out", sizeof("--out") - 1)) {
+                p++;
+                mkdir_p_and_touch(*p);
+                return 0;
+            }
+        }
+        mkdir_p_and_touch(argv[argc - 1]);
+        return 0;
     }
 
-    FILE *f = fopen(ptr, "w");
-    fclose(f);
+    while (strncmp(*argv, "-o", sizeof("-o") - 1) && strncmp(*argv, "--out", sizeof("--out") - 1)) {
+        argv++;
+    }
+    argv++;
+    mkdir_p(*argv);
+    for(char **p = argv + 1; *p; p++) {
+        char path[PATH_MAX + 1];
+        char *ptr = stpncpy(path, *argv, PATH_MAX >> 1);
+        strncpy(path, *p, PATH_MAX >> 1);
+        FILE *f = fopen(path, "w");
+        fclose(f);
+    }
     return 0;
 }
