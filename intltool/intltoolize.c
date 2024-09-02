86933c715b5412ee256d752fddc72e412bb31995
stefan11111 <stefan11111@shitposting.expert>
2024-09-03 02:37:30 +0300

    update intltool shim

--- intltool/intltoolize.c
+++ intltool/intltoolize.c
@@ -6,9 +6,11 @@ int main(int argc, char **argv)
     for (char **p = argv; *p; p++) {
         if (!strncmp(*p, "--force", sizeof("--force") - 1)) {
             FILE *f = fopen("po/Makefile.in.in", "a");
-            if (f) {
-                fclose(f);
+            if (!f) {
+                return 0;
             }
+            fprintf(f, "%s", "# INTLTOOL_MAKEFILE\n");
+            fclose(f);
             return 0;
         }
     }
